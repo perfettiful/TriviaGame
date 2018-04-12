b4052e6b6c847f55001383b321789c101c0bc9ca
Nate <nathanperfetti@gmail.com>
2018-04-12 18:54:06 -0500

    Initial Commit with File Structure and assets

---
+++ assets/javascript/app.js
@@ -0,0 +1,92 @@
+/****Geography Trivia Game by Nathan Perfetti 4-12-18 **********/
+
+//Begin by importing Question Set JSON
+
+var quiz = [
+    {
+    "question": "How many federal states does Germany have?",
+    "correct_answer": "16",
+    "incorrect_answers": [
+    "13",
+    "32",
+    "25"
+    ]},
+    {
+    "question": "What colour is the circle on the Japanese flag?",
+    "correct_answer" : "Red",
+    "incorrect_answers": [
+    "White",
+    "Yellow",
+    "Black"]},
+    {
+    "question": "What is the capital of Jamaica?",
+    "correct_answer": "Kingston",
+    "incorrect_answers": [
+    "Rio de Janeiro",
+    "Dar es Salaam",
+    "Kano"]},
+    {
+    "question": "What is the capital of the US State of New York?",
+    "correct_answer": "Albany",
+    "incorrect_answers": [
+    "Buffalo",
+    "New York",
+    "Rochester"]},
+    {
+    "question": "Which country is the home of the largest Japanese population outside of Japan?",
+    "correct_answer": "Brazil",
+    "incorrect_answers": [
+    "China",
+    "Russia",
+    "The United States"
+    ]
+    },
+    {
+    "question": "How many countries does Mexico border?",
+    "correct_answer": "3",
+    "incorrect_answers": [
+    "2",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "question": "Which of the following Japanese islands is the biggest?",
+    "correct_answer": "Honshu",
+    "incorrect_answers": [
+    "Hokkaido",
+    "Shikoku",
+    "Kyushu"
+    ]
+    },
+    {
+    "question": "What is the capital of India?",
+    "correct_answer": "New Delhi",
+    "incorrect_answers": [
+    "Bejing",
+    "Montreal",
+    "Tithi"
+    ]
+    },
+    {
+    "question": "What is the smallest country in the world?",
+    "correct_answer": "Vatican City",
+    "incorrect_answers": [
+    "Maldives",
+    "Monaco",
+    "Malta"
+    ]
+    },
+    {
+    "question": "Which of the following countries has a flag featuring a yellow lion wielding a sword on a dark red background?",
+    "correct_answer": "Sri Lanka",
+    "incorrect_answers": [
+    "Kiribati",
+    "Scotland",
+    "Bhutan"
+    ]
+    }];
+
+    console.log(quiz[1].question);
+
+    
